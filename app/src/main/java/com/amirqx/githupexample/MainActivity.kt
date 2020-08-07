0371a441cff283da6d98be3060c3be6eeab7be3f
amirqx <vhsbhv@yahoo.com>
2020-08-07 18:05:00 +0430

    this is testtt

---
+++ app/src/main/java/com/amirqx/githupexample/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.amirqx.githupexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
